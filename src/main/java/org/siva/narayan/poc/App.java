a8a22855c225e8f85f461ccd7ec3a3f9c4fc9f4c
Siva Narayana Reddy Maddula <siva_narayanareddy_maddula@apple.com>
2015-12-03 17:12:42 +0530

    initial commit

---
+++ src/main/java/org/siva/narayan/poc/App.java
@@ -0,0 +1,13 @@
+package org.siva.narayan.poc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
